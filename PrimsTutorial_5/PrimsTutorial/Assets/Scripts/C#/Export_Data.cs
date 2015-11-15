d308b949e10966cca32f9903bc0e57c7c82c7a55
Shiro <shiro.archerl@gmail.com>
2015-11-15 01:30:29 -0600

    casi esta
    Aun faltan las diagonales y el export de data

---
+++ PrimsTutorial_5/PrimsTutorial/Assets/Scripts/C#/Export_Data.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Export_Data : MonoBehaviour {
+    List<float> mazeTimes = new List<float>();
+    List<float> pathTimes = new List<float>();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
